1e888eb65c6dd01a9381e4a61aef9147074572df
paty-2310 <patriciamayora86@gmail.com>
2025-10-04 16:10:33 -0400

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$nombre = "patricia" 
-$apellido = "portillo" 
-$numero1 = 8.5 
-$numero2 = 8.5 
-echo "hola mundo, mi nombre es:" . ($nombre + $apellido). "\n" 
-echo "y tengo:" . ($numero1 + $numero2)
